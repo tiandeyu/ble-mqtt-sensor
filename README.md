6f6299b1116cdc11340cdba0635d9ec01e73fece
natic <deyu.tian@enmotech.com>
2019-06-14 18:46:56 +0800

    update config

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-##BLE MQTT Sensor 
+## BLE MQTT Sensor 
 
 
 >BLE MQTT Sensor是一款ha自定义插件,配合esp32-ble2mqtt使用,能够整合mqtt信息到homeassistant
 
 
-###下载custom component
+### 下载custom component
 下载custom_components下面所有文件到如下目录/config/custom_components/
 
 ```
@@ -14,21 +14,25 @@
 /config/custom_components/ble_mqtt_sensor/manifest.json
 ```
 
-###configuration.yaml配置
+### configuration.yaml配置
 name: ha中显示传感器的名字  
 mac: 小写mac地址  
-scan_interval: 扫描间隔s，可选配置，默认30   
+topic: 可选配置，ble2mqtt config.json里面配置的service name和characteristics name, 默认/SensorService/SensorValue 
+set_suffix: 可选配置，ble2mqtt config.json里面配置的set_suffix，默认/Set
+scan_interval: 可选配置，扫描间隔s，默认30   
 device_class: meizu_remote代表魅族遥控器
 ```yaml
 sensor:
   - platform: ble_mqtt_sensor
     name: 'Meizu Remote'
     mac: '68:3e:34:cc:d4:69'
+    topic: '/SensorService/SensorValue'
+    set_suffix: '/Set'
     scan_interval: 60
     device_class: meizu_remote
 ```
 
-###多个配置
+### 多个配置
 ```yaml
 sensor:
   - platform: ble_mqtt_sensor
--- custom_components/ble_mqtt_sensor/sensor.py
+++ custom_components/ble_mqtt_sensor/sensor.py
@@ -10,11 +10,14 @@ from homeassistant.const import (TEMP_CELSIUS,
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import (PLATFORM_SCHEMA)
 from homeassistant.util import Throttle
-import json
 
 _LOGGER = logging.getLogger(__name__)
 
+CONF_TOPIC = "topic"
+CONF_SET_SUFFIX = "set_suffix"
 SCAN_INTERVAL = timedelta(seconds=30)
+DEFAULT_TOPIC = "/SensorService/SensorValue"
+DEFAULT_SET_SUFFIX = "/Set"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
@@ -26,46 +29,69 @@ ATTR_TEMPERATURE = 'Temperature'
 ATTR_HUMIDITY = 'Humidity'
 ATTR_BATTERY = 'Battery'
 
+
 def setup_platform(hass, config, add_devices, discovery_info=None):
     name = config.get(CONF_NAME)
     mac_address = config.get(CONF_MAC)
+    topic = config.get(CONF_TOPIC, DEFAULT_TOPIC)
+    set_suffix = config.get(CONF_SET_SUFFIX, DEFAULT_SET_SUFFIX)
     update_interval = config.get(CONF_SCAN_INTERVAL, SCAN_INTERVAL)
     add_devices([
-        MeizuTemperature(hass, name, mac_address, update_interval),
+        MeizuTemperature(hass, name, mac_address, update_interval, topic, set_suffix),
         MeizuHumidity(hass, name, mac_address, update_interval),
-        MeizuBattery(hass, name, mac_address, timedelta(hours=12)),
     ])
 
+
 class MeizuTemperature(Entity):
-    def __init__(self, hass, name, mac_address, interval):
+    def __init__(self, hass, name, mac_address, interval, topic, set_suffix):
         """Initialize the generic Xiaomi device."""
         self._hass = hass
         self._name = name + ' ' + ATTR_TEMPERATURE
         self._mac_address = mac_address
+        self._topic = topic
+        self._set_suffix = set_suffix
         self._state = None
         self._state_attrs = {
-            'humidity': 0
+            ATTR_HUMIDITY: None,
+            ATTR_BATTERY: None
         }
         self._update()
+        self.update_battery_level()
         self.update = Throttle(interval)(self._update)
 
     def _update(self):
         payload = '85,3,8,17'
-        sub_topic = self._mac_address + '/SensorService/SensorValue'
-        pub_topic = sub_topic + '/Set'
+        sub_topic = self._mac_address + self._topic
+        pub_topic = sub_topic + self._set_suffix
         mqtt.publish(self._hass, pub_topic, payload)
 
         @callback
         def msg_callback(msg):
             """Receive events published by and fire them on this hass instance."""
             results = msg.payload.split(',')
-            #返回数据8位是温湿度信息
+            # 返回数据8位是温湿度信息
             if len(results) == 8:
                 self._state = round((int(results[4]) + int(results[5]) * 16 * 16) / 100, 1)
-                self._state_attrs[ATTR_HUMIDITY.lower()] = round((int(results[6]) + int(results[7]) * 16 * 16) / 100, 1)
+                self._state_attrs[ATTR_HUMIDITY] = round((int(results[6]) + int(results[7]) * 16 * 16) / 100, 1)
 
         mqtt.subscribe(self._hass, sub_topic, msg_callback)
 
+    @Throttle(timedelta(hours=12))
+    def update_battery_level(self):
+        payload = '85,3,1,16'
+        sub_topic = self._mac_address + self._topic
+        pub_topic = sub_topic + self._set_suffix
+        mqtt.publish(self._hass, pub_topic, payload)
+
+        @callback
+        def msg_callback(msg):
+            """Receive events published by and fire them on this hass instance."""
+            results = msg.payload.split(',')
+            # 返回数据5位是电量信息
+            if len(results) == 5:
+                self._state_attrs[ATTR_BATTERY] = round(int(results[4]) / 10, 1)
+
+        mqtt.subscribe(self._hass, sub_topic, msg_callback)
 
     @property
     def name(self):
@@ -98,7 +124,7 @@ class MeizuHumidity(Entity):
         """Initialize the generic Xiaomi device."""
         self._hass = hass
         self._name = name + ' ' + ATTR_HUMIDITY
-        self._entity_id = "sensor." + name.lower() + "_" + ATTR_TEMPERATURE.lower()
+        self._entity_id = "sensor." + name + "_" + ATTR_TEMPERATURE
         self._mac_address = mac_address
         self._state = None
         self._update()
@@ -110,7 +136,7 @@ class MeizuHumidity(Entity):
             nowtime = datetime.utcnow()
             updated_time = stat.last_updated.replace(tzinfo=None)
             if nowtime - updated_time < timedelta(minutes=3):
-                self._state = stat.attributes[ATTR_HUMIDITY.lower()]
+                self._state = stat.attributes[ATTR_HUMIDITY]
 
     @property
     def name(self):
@@ -132,49 +158,3 @@ class MeizuHumidity(Entity):
         """Return the device class of this entity."""
         return DEVICE_CLASS_HUMIDITY
 
-class MeizuBattery(Entity):
-    def __init__(self, hass, name, mac_address, interval):
-        """Initialize the generic Xiaomi device."""
-        self._hass = hass
-        self._name = name + ' ' + ATTR_BATTERY
-        self._mac_address = mac_address
-        self._state = None
-        self._update()
-        self.update = Throttle(interval)(self._update)
-
-    def _update(self):
-        payload = '85,3,1,16'
-        sub_topic = self._mac_address + '/SensorService/SensorValue'
-        pub_topic = sub_topic + '/Set'
-        mqtt.publish(self._hass, pub_topic, payload)
-
-        @callback
-        def msg_callback(msg):
-            """Receive events published by and fire them on this hass instance."""
-            results = msg.payload.split(',')
-            # 返回数据5位是电量信息
-            if len(results) == 5:
-                self._state = int(results[4]) / 10
-
-        mqtt.subscribe(self._hass, sub_topic, msg_callback)
-
-
-    @property
-    def name(self):
-        """Return the name of the device if any."""
-        return self._name
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return 'V'
-
-    @property
-    def device_class(self):
-        """Return the device class of this entity."""
-        return DEVICE_CLASS_BATTERY
